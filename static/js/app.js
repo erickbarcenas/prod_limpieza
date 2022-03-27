5903fb8efc7b741f81f8f77de029d836736a8f1f
Erick Bárcenas <barcenas.dev@gmail.com>
2022-03-27 08:05:11 -0600

    contact - js css working!

--- static/js/app.js
+++ static/js/app.js
@@ -128,10 +128,8 @@ burgerMenu.addEventListener('click',function(){
 });
 
 /** ====================================
-*  🔰 Index - scroll
+*  🔰 Index - Anchor
   ==================================== **/
-
-
 
 function go_to_anchor(anchor) {
     var loc = document.location.toString().split('#')[0];
@@ -145,3 +143,32 @@ function go_to_anchor(anchor) {
     });
     return false;
 }
+
+/** ====================================
+*  🔰 Index - Contact
+  ==================================== **/
+
+const contactButtons = document.querySelectorAll(".js-contactButton"),
+    contactContents = document.querySelectorAll(".js-contactContent"),
+    contactWrapper = document.querySelector(".js-contactWrapper");
+
+    window.addEventListener("DOMContentLoaded", (()=>{
+        contactButtons.forEach((t=>{
+            t.addEventListener("click", (t=>{
+                const o = t.target.dataset.name;
+                contactButtons.forEach((o=>{
+                    o.classList.remove("is-show"),
+                    t.target.classList.add("is-show")
+                }
+                )),
+                contactContents.forEach((t=>{
+                    t.classList.remove("is-show"),
+                    t.dataset.name == o && t.classList.add("is-show")
+                }
+                ))
+            }
+            ))
+        }
+        ))
+    }
+));
