9fe0f37c4bbea0615f433e07994bee0ad759e450
Erick Bárcenas <barcenas.dev@gmail.com>
2022-03-26 20:44:40 -0600

    [ADD SEARCH INPUT] working!

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,7 @@
-
+/** ====================================
+*  🔰 Index - Carousel
+  ==================================== **/
+/*
 window.addEventListener('load', ()=>{
     console.log("init carousel");
     let idx_imgs = 0;
@@ -21,3 +24,55 @@ window.addEventListener('load', ()=>{
 
     setInterval(change_img, time);
 })
+*/
+/** ====================================
+*  🔰 Index - Show Modal: products and whatsapp link
+  ==================================== **/
+const targetDiv = document.getElementById("modal__index");
+const show_products = () => {
+    if (targetDiv.style.display !== "none") {
+        targetDiv.style.display = "none";
+        scroll(0,0);
+    } else {
+        targetDiv.style.display = "block";
+    }
+}
+
+/** ====================================
+*  🔰 Index - Input seach product
+  ==================================== **/
+
+  let cards = document.querySelectorAll('.card')
+    
+  function liveSearch() {
+      let search_query = document.getElementById("searchbox").value;
+      
+      //Use innerText if all contents are visible
+      //Use textContent for including hidden elements
+      for (var i = 0; i < cards.length; i++) {
+          if(cards[i].textContent.toLowerCase()
+                  .includes(search_query.toLowerCase())) {
+              cards[i].classList.remove("is-hidden");
+          } else {
+              cards[i].classList.add("is-hidden");
+          }
+      }
+  }
+  
+  //A little delay
+  let typingTimer;               
+  let typeInterval = 500;  
+  let searchInput = document.getElementById('searchbox');
+  
+  searchInput.addEventListener('keyup', () => {
+      clearTimeout(typingTimer);
+      typingTimer = setTimeout(liveSearch, typeInterval);
+  });
+
+  /* */
+
+  const seach_product = () => {
+    /*console.log("searching...");*/
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(liveSearch, typeInterval);
+  }
