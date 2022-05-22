bedae44691f3c5999da32007412f2539bca04294
barcenas <barcenas.developer@gmail.com>
2022-05-22 03:54:01 -0500

    done success page

--- success.php
+++ success.php
@@ -10,7 +10,11 @@
 <?php require 'partials/header.php' ?>
 <br><br><br><br><br><br>
 
-<section>
+<section class="container_success">
+    
+    <br><br>
+    <img class="success_png" src="./static/imgs/resources/success.png" alt="shopping_cart">
+    <br><br>
     <h1> Gracias por tu compra </h1>
 </section>
 
